eca9a7095bba20d9d5c5afa7b6513c95b1889081
rosibes <seby.boiciuc03@e-uvt.ro>
2025-06-22 15:39:00 +0300

    changed the code

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.get("/users", (req, res) => {
   prismaClient.user.findMany()
     .then(users => {
-      res.json({users});
+      res.json(users);
     })
     .catch(err => {
       res.status(500).json({ error: err.message });
@@ -17,7 +17,7 @@ app.get("/users", (req, res) => {
 
 app.post("/user", (req, res) => {
   const { username, password } = req.body;
-  
+
   if (!username || !password) {
     res.status(400).json({ error: "Username and password are required" });
     return
